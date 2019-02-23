cf93b1da5693b3887786def194930b210676255d
Katie DeAngelis <katie.e.deangelis@gmail.com>
2019-02-23 16:21:34 -0500

    Integrated Material-US for styling.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/application';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+    },
+    typography: { useNextVariants: true },
+  });
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
